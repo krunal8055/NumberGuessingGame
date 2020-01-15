2d2bf6940ed712aefc39cfcbe632aa859a6f5a21
bingisai <bingisai5@gmail.com>
2020-01-15 06:45:22 -0400

    Bug Fixes And Some Changes

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -2,54 +2,142 @@
 
 namespace NumberGuessingGame
 {
-    class Program
-    {
+   class Program
+   {
+        static void Main(String[] args)
+        {
+            Starting();             //Method For App NAME and VERSION
 
-        static void Main(string[] args)
+            int min, max;           //Two Variable Declaration
+
+            //MinNumberFromUser
+            while (true)
+            {
+                //Min Number
+                ChangeColor("Please Enter Min Number: ", ConsoleColor.Yellow);
+                String MIN = Console.ReadLine();
+
+                //Condition to Check User Enters Number or NOT
+                if (!int.TryParse(MIN, out min))
+                {
+                    ChangeColor("Please Enter Valid Min Number!", ConsoleColor.Red);
+                    continue;
+                }
+                else
+                {
+                    min = Int32.Parse(MIN);
+                    break;
+                }
+            }
+            //MaxNumberFromUser
+            while (true)
+            {
+                //Max Number
+                ChangeColor("Please Enter Max Number: ", ConsoleColor.Yellow);
+                String MAX = Console.ReadLine();
+
+                //Condition to Check User Enters Number or NOT
+                if (!int.TryParse(MAX, out max))
+                {
+                    ChangeColor("Please Enter Valid MAX Number!", ConsoleColor.Red);
+                    continue;
+                }
+                else
+                {
+                    max = Int32.Parse(MAX);
+                    break;
+                }
+            }
+
+            //Boolean Var To Check Min and Max Values are appropriate for Game
+            bool minMaxCheck = CheckMinMax(min,max);
+            if (minMaxCheck == true)
+            {
+                furtherGame(min, max);
+            }
+            else
+            {
+                ChangeColor("Something is Wrong.Please Try Again.", ConsoleColor.Red);
+            }
+        }
+
+        //Method For Starting Statements
+        static void Starting()
         {
             String GameName = "Number Guessing Game";
             String GameVersion = "1.0.0";
-            Console.WriteLine("Hello And Welcome to {0} Version:{1}",GameName,GameVersion);
+            ChangeColor("Welcome to "+GameName, ConsoleColor.Green);
+            ChangeColor("Version:" + GameVersion, ConsoleColor.DarkGreen);
+        }
 
+        //Methods for Check Min AND Max Number
+        static bool CheckMinMax(int min, int max)
+        {
+            if (min < max && min != max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
-            Console.WriteLine("Hint Guess Number Between 1 - 20.");
+        //Method Of Real Game
+        static void furtherGame(int min, int max)
+        {
+            //Hint
+            Console.WriteLine("Hint: Guess Number Between {0} and {1}.",min,max);
+
             while (true)
             {
+                //Random Number Generator
                 Random random = new Random();
-                int Correct_Answer = random.Next(0,20);
+                int Correct_Ans = random.Next(min, max);
 
-                    Console.Write("Please Enter Your Guess: ");
-                    String UserGuess = Console.ReadLine();
-                int UserInput;
-                if (!int.TryParse(UserGuess, out UserInput))
+                //User's Guess
+                ChangeColor("Please Enter Your Guess : ", ConsoleColor.Yellow);
+                String userGuess = Console.ReadLine();
+                int user_Guess;
+
+                //Check USER input is Int or not
+                if (!int.TryParse(userGuess, out user_Guess))
                 {
-                    Console.WriteLine("Bad INPUT.Please Try With Integer Values");
+                        ChangeColor("Please Try Again With Integer Guess!", ConsoleColor.Red);
+                        continue;
                 }
-                else {
-
-                        if (Correct_Answer != Int32.Parse(UserGuess))
+                else if(user_Guess >= min && user_Guess <= max)
+                {
+                        //Match the Correct Ans with User's Guess
+                        if (Correct_Ans != user_Guess)
                         {
-                            if (Int32.Parse(UserGuess) < 0 || Int32.Parse(UserGuess) > 20)
-                            {
-                                Console.WriteLine("Please Guess inbetween 0-20");
-                                continue;
-                            }
-                            else 
-                            {
-                                Console.WriteLine("Wrong Answer. Try Again!");
-                                continue;
-                            }
-                            
+                            ChangeColor("Please Try  Again!", ConsoleColor.Red);
+                            continue;
                         }
-                        if(Correct_Answer == Int32.Parse(UserGuess))
+                        else if (Correct_Ans == user_Guess)
                         {
-                            Console.WriteLine("Correct Answer.You are Guiness!");
+                            ChangeColor("You Got it! Guiness!", ConsoleColor.DarkMagenta);
                             return;
                         }
                 }
-               
-            }
+                else
+                {
+                    ChangeColor("Please Try Number Inbetween " + min + " - " + max + " !", ConsoleColor.Red);
+                    continue;
+                }
             
+            }
         }
-    }
+
+        //Method For Print Messages with Different Font Color
+        static void ChangeColor(String text, ConsoleColor color)
+        {
+            
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+
+
+   }
 }
